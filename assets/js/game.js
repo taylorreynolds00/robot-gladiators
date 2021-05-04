896df19f586f412a67aec21cd15894fbad56ba82
TJR <tayzer10@gmail.com>
2021-05-04 01:40:06 -0600

    naming my robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  var playerName = window.prompt("what is your robot's name?");
+// What is this?
+    console.log(playerName);
+    console.log("this logs a string, good for leaving yourself a message");
+    //this will do math and log 20
+    console.log(10+10);
+    //what is this
+    console.log("Our robot's name is " + playerName);
+
+
+  // fight();
